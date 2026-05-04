d2288d2838984200b23d8961b41f2680fe235e0b
Michael Linhardt <linhardt.michael@gmail.com>
2026-05-04 17:17:13 +0700

    checkpoint

--- zsh/alias/checkpoint.zsh
+++ zsh/alias/checkpoint.zsh
@@ -1,7 +1,9 @@
 checkpoint () {
+	echo $CW8"[ checkpoint ] ~/workspace"$CWH
 	cd ~/workspace
     gad -A && gco "checkpoint" && gpu
     cd - > /dev/null
+	echo $CW8"[ checkpoint ] ~/secondbrain"$CWH
     cd ~/secondbrain
     gad -A && gco "checkpoint" && gpu
 	cd - > /dev/null
